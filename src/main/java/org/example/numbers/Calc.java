cfddb4ec5c6265c097024e9517757abf8702ad67
sneharani1818 <sneharani1818@gmail.com>
2025-12-30 15:10:16 +0530

    simple junit testing

---
+++ src/main/java/org/example/numbers/Calc.java
@@ -0,0 +1,11 @@
+package org.example.numbers;
+
+public class Calc {
+    //one unit of application
+    public int sum(int a, int b){
+        return a+b;
+    }
+    public int diff(int a, int b){
+        return Math.abs(a-b);
+    }
+}
---
+++ src/test/java/org/example/numbers/CalcTest.java
@@ -0,0 +1,21 @@
+package org.example.numbers;
+
+import junit.framework.TestCase;
+
+public class CalcTest extends TestCase {
+
+    public void testSum() {
+        //        fail("Not yet implemented");
+        System.out.println("First JUnit ran");
+        Calc c= new Calc();
+        int actual=c.sum(3,5);
+        int expected=8;
+        assertEquals(expected, actual);
+    }
+
+    public void testDiff() {
+        Calc c= new Calc();
+        assertEquals(4,new Calc().diff(4,8));
+
+    }
+}
